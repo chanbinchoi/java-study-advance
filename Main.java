91fb6df694897546ea7eb07bc44003796d9ac752
chanbinchoi <classy9611@gmail.com>
2026-03-01 23:12:13 +0900

    ~Project 1- Hard Coded Questions and Display them

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
-package AdvanceJava;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        QuestionService service = new QuestionService();
+        service.displayQuestions();
     }
 }
