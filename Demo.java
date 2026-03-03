574e39a1d8d1a3f46d568f65db387509ae742ff6
chanbinchoi <classy9611@gmail.com>
2026-03-03 23:26:17 +0900

    throw

--- Demo.java
+++ Demo.java
@@ -1,19 +1,17 @@
-class A {
-    public void showTheDataWhichBelongsToThisClass() {
-        System.out.println("in A show");
-    }
-}
-
-class B extends A {
-    @Override
-    public void showTheDataWhichBelongsToThisClass() {
-        System.out.println("in B show");
-    }
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Demo {
-    public static void main(String[] args) {
-        B obj = new B();
-        obj.showTheDataWhichBelongsToThisClass();
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int num = 0;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+        } finally {
+            br.close();
+        }
     }
 }
