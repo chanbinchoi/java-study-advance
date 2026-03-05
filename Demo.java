cda7d8c3eb960c9421f6298a59b4c9afa1364fad
Chanbin Choi <classy9611@gmail.com>
2026-03-05 13:42:10 +0900

    update demo

--- Demo.java
+++ Demo.java
@@ -1,17 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Demo {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        int num = 0;
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new InputStreamReader(System.in));
-            num = Integer.parseInt(br.readLine());
-            System.out.println(num);
-        } finally {
-            br.close();
-        }
+    public static void main(String[] args) {
+
     }
 }
